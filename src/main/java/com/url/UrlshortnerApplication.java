85ed51aa0b600d4d981bc066a7c531d4d517515c
surabhiverma78388 <aloksurabhivermaofficial@gmail.com>
2026-01-21 22:40:54 +0530

    Configure JSP for Railway deployment: WAR packaging, env vars, ServletInitializer

--- src/main/java/com/url/UrlshortnerApplication.java
+++ src/main/java/com/url/UrlshortnerApplication.java
@@ -2,9 +2,16 @@ package com.url;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UrlshortnerApplication {
+public class UrlshortnerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UrlshortnerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlshortnerApplication.class, args);
